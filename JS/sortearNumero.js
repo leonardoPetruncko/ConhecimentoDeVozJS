ed9efd3416b76c7b518b4ee12606cadddd4baec2
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-12 15:10:12 -0300

    Testando math.random

--- JS/sortearNumero.js
+++ JS/sortearNumero.js
@@ -1,1 +1,10 @@
-console.log("dale");
+const numeroSecreto = gerarNumeroAleatorio() 
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100)
+}
+
+console.log(numeroSecreto);
+
+gerador2() => parseInt(Math.random() * 100);
+
